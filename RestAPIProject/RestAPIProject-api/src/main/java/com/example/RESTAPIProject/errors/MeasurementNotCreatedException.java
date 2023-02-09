3979a4342a5e2bac30a351d1ba48017c7c89e736
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-02-09 13:40:52 +0200

    Delete RestAPIProject directory

--- RestAPIProject/RestAPIProject-api/src/main/java/com/example/RESTAPIProject/errors/MeasurementNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.RESTAPIProject.errors;
-
-public class MeasurementNotCreatedException extends RuntimeException{
-    public MeasurementNotCreatedException(String message) {
-        super(message);
-    }
-}
