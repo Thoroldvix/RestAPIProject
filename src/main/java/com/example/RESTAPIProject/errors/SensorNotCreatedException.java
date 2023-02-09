504661ef07a44eed76519a14d9abf187c40a4b73
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-02-09 13:37:39 +0200

    Delete src directory

--- src/main/java/com/example/RESTAPIProject/errors/SensorNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.RESTAPIProject.errors;
-
-public class SensorNotCreatedException extends RuntimeException {
-    public SensorNotCreatedException(String message) {
-        super(message);
-    }
-}
